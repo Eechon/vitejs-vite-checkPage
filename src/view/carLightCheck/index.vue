68f98a34981f6c768c79222aaaea71562a7c9e41
Eechon <545429204@qq.com>
2024-07-03 10:34:44 +0800

    初始化

--- src/view/carLightCheck/index.vue
+++ src/view/carLightCheck/index.vue
@@ -1,15 +1,8 @@
 <script setup></script>
 
 <template>
-  <div>this is carlightcheck page</div>
-  <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </div>
+  <div class="top"></div>
+    
 </template>
 
 <style></style>
