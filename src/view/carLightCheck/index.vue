07c395891ec380471381cd720abe9e8168d720c0
eechon <545429204@qq.com>
2024-07-04 17:25:36 +0800

    添加宽高

--- src/view/carLightCheck/index.vue
+++ src/view/carLightCheck/index.vue
@@ -1,27 +1,37 @@
-<script setup></script>
-
 <template>
   <div class="container">
     <div class="top">
     </div>
-    <div class="middle"></div>
+    <div class="middle">this is carlight page</div>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+
 <style>
 .container {
-  position: relative;
+  /* position: relative; */
   background-color: #999;
 }
 
 .top {
   height: 60px;
+  width: 100%;
   background-color: #ff5a01;
 }
 
 .middle {
   margin-top: 60px;
   height: 100vh-60px;
+  width: 100%;
   background-color: pink;
 }
 </style>
